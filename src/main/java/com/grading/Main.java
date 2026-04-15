178a491bed884256b01ab78ed977bf4d36a3a940
Tony Tony Chopper <241455787+toni-toni-choppa@users.noreply.github.com>
2026-04-15 11:49:33 -0400

    Refactor Teacher and Student for ArrayList

--- src/main/java/com/grading/Main.java
+++ src/main/java/com/grading/Main.java
@@ -1,5 +1,6 @@
 package com.grading;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -7,8 +8,13 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         // Create instances of Student and Teacher
-        Student student = new Student("johnd", "John Doe");
-        Teacher teacher = new Teacher("janed", "Mrs. Jane Doe");
+        ArrayList<Student> students = new ArrayList<>();
+        ArrayList<Teacher> teachers = new ArrayList<>();
+
+        // Add some sample students / teachers
+        students.add(new Student("johnd", "John Doe"));
+        teachers.add(new Teacher("janed", "Mrs. Jane Doe"));
+
 
         String username = null;
 
@@ -18,30 +24,22 @@ public class Main {
 
             String input = in.nextLine();
 
-            // Placeholder for determining if username exists
-            switch (input) {
-                case "johnd":
-                    username = input;
-                    break;
-                case "janed":
-                    username = input;
-                    break;
-                default:
-                    System.out.println("[ERROR] User not found. Please try again.");
+            for (Student s : students) {
+                if (s.getUsername().equals(input)) {
+                    username = s.getUsername();
+                    System.out.println("Welcome, " + s.getName() + "!");
+                }
             }
+
+            for (Teacher t : teachers) {
+                if (t.getUsername().equals(input)) {
+                    username = t.getUsername();
+                    System.out.println("Welcome, " + t.getName() + "!");
+                    break;
+                }
+            }
+
+            System.out.println("[ERROR] User not found. Please try again.");
         } while (username == null);
-
-        // Print menu for options after login
-        switch (username) {
-            case "johnd":
-                System.out.println("Welcome, " + student.getName() + "!");
-                // Placeholder for student options
-                break;
-            case "janed":
-                System.out.println("Welcome, " + teacher.getName() + "!");
-                // Placeholder for teacher options
-                break;
-        }
-
     }
 }
